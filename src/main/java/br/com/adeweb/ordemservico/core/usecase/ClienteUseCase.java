a2d03d058587166f9382f2d438f9b37e09fc7b86
emersonpessoa <eps011108@gmail.com>
2025-09-30 12:17:37 -0300

    feat: atualizado :rocket:

--- src/main/java/br/com/adeweb/ordemservico/core/usecase/ClienteUseCase.java
+++ src/main/java/br/com/adeweb/ordemservico/core/usecase/ClienteUseCase.java
@@ -9,8 +9,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
 public class ClienteUseCase implements ClienteInputPort {
 
