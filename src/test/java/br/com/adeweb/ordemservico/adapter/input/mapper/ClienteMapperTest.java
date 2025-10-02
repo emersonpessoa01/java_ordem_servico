7ceef844bb2c3ff3667eb45a75ce516b7077d816
emersonpessoa <eps011108@gmail.com>
2025-10-02 16:37:02 -0300

    test: Adicionado comentários e o método deveMapearEntidadeParaDominio :hammer:

--- src/test/java/br/com/adeweb/ordemservico/adapter/input/mapper/ClienteMapperTest.java
+++ src/test/java/br/com/adeweb/ordemservico/adapter/input/mapper/ClienteMapperTest.java
@@ -2,6 +2,7 @@ package br.com.adeweb.ordemservico.adapter.input.mapper;
 
 
 import br.com.adeweb.ordemservico.adapter.input.request.ClienteRequest;
+import br.com.adeweb.ordemservico.adapter.output.entities.ClienteEntity;
 import br.com.adeweb.ordemservico.core.domain.model.Cliente;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -19,14 +20,30 @@ public class ClienteMapperTest {
 
     @Test
     void deveMapearRequestParaDominio() {
+        // Give: Um cliente request preenchido
         ClienteRequest request = new ClienteRequest();
         request.setNome("José");
         request.setEmail("jose@email.com");
 
+        // When: Converter para domínio com mapper
         Cliente cliente = clienteMapper.toDomainFromRequest(request);
 
+        // Then: Verificar se os campos foram mapeados corretamente
         assertEquals("José", cliente.getNome());
         assertEquals("jose@email.com", cliente.getEmail());
+    }
+    @Test
+    void deveMapearEntidadeParaDominio(){
+        // Given: ClienteEntity disponível
+        ClienteEntity entity = new ClienteEntity(1L, "Maria", "maria@gmail.com");
+
+        // When: Converter para domínio com mapper
+        Cliente cliente = clienteMapper.toDomainEntity(entity);
+
+        // Then: Verificar se os campos foram mapeados corretamente
+        assertEquals(1L, cliente.getId());
+        assertEquals("Maria", cliente.getNome());
+        assertEquals("maria@gmail.com", cliente.getEmail());
     }
 
 }
