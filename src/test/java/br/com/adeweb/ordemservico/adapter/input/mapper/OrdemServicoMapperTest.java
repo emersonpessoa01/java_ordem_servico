bd5d62abe1e4e8cf8d048e63ac063d97a0e2c08f
emersonpessoa <eps011108@gmail.com>
2025-10-02 22:36:32 -0300

    test:Adicionado deveMapearEntidadeParaDominio :tools:

--- src/test/java/br/com/adeweb/ordemservico/adapter/input/mapper/OrdemServicoMapperTest.java
+++ src/test/java/br/com/adeweb/ordemservico/adapter/input/mapper/OrdemServicoMapperTest.java
@@ -1,6 +1,7 @@
 package br.com.adeweb.ordemservico.adapter.input.mapper;
 
 import br.com.adeweb.ordemservico.adapter.input.request.OrdemServicoRequest;
+import br.com.adeweb.ordemservico.adapter.output.entities.OrdemServicoEntity;
 import br.com.adeweb.ordemservico.core.domain.model.OrdemServico;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -8,8 +9,10 @@ import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class OrdemServicoMapperTest {
     private static OrdemServicoMapper ordemServicoMapper;
@@ -35,5 +38,25 @@ public class OrdemServicoMapperTest {
         assertEquals(1L, ordemServico.getClienteId());
         assertEquals("Descrição", ordemServico.getDescricao());
         assertEquals(new BigDecimal("100.50"), ordemServico.getValor());
+    }
+    @Test
+    void deveMapearEntidadeParaDominio(){
+        // Given: OrdemServicoEntity preenchida
+        OrdemServicoEntity entity = new OrdemServicoEntity();
+        entity.setId(1L);
+        entity.setClienteId(1L);
+        entity.setDescricao("Descrição");
+        entity.setValor(new BigDecimal("200.00"));
+        entity.setAbertoEm(LocalDateTime.now());
+
+        // When:Converter para domínio
+        OrdemServico ordemServico = ordemServicoMapper.toDomainFromEntity(entity);
+
+        // Then:  Verificar dados convertidos
+        assertEquals(1L, ordemServico.getId());
+        assertEquals("Descrição", ordemServico.getDescricao());
+        assertEquals(new BigDecimal("200.00"), ordemServico.getValor());
+        assertNotNull(ordemServico.getAbertoEm());
+
     }
 }
