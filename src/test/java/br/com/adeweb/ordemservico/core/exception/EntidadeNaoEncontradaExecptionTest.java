cc21acad79dea6cb6cc467975c3b98905e27587b
emersonpessoa <eps011108@gmail.com>
2025-10-03 21:12:47 -0300

    test: Realizado teste do EntidadeNaoEncontradaExecptionTest :tools:

---
+++ src/test/java/br/com/adeweb/ordemservico/core/exception/EntidadeNaoEncontradaExecptionTest.java
@@ -0,0 +1,20 @@
+package br.com.adeweb.ordemservico.core.exception;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class EntidadeNaoEncontradaExecptionTest {
+
+    @Test
+    void deveCriarexceptionComMensagem(){
+        // Given: Uma mensagem simulando erro de entidade não encontrado
+        String mensagem = "Entidade não localizada no sistema";
+
+        // When: Instancia a exceção passando a mensagem
+        EntidadeNaoEncontradaExecption exception = new EntidadeNaoEncontradaExecption(mensagem);
+
+        // Then: Verifica se a mensagem foi armazenada corretamente pela RuntimeException
+       assertEquals(mensagem, exception.getMessage());
+    }
+}
